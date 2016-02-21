d93f2ae40d5bbf30b0aa1c377978548c1d3c1e12
Ronald Hernandez <rhernandez@solstice-mobile.com>
2016-02-21 11:45:24 -0600

    set up controlls

---
+++ AddPostVC.swift
@@ -0,0 +1,19 @@
+//
+//  AddPostVC.swift
+//  MyHoodApp
+//
+//  Created by Ronald Hernandez on 2/14/16.
+//  Copyright © 2016 Solstice. All rights reserved.
+//
+
+import UIKit
+
+class AddPostVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
